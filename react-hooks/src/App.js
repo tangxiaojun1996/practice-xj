0e9d928948ab2d2a225b2259956aefaf57c6f583
“xj.tang” <xj.tang@wanwuyunlian.com>
2021-02-22 23:29:40 +0800

    feat: react-hooks: context

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Context, SetContext } from "./index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Consumer>
+      {(contextState) => {
+        const { count } = contextState;
+        return (
+          <SetContext.Consumer>
+            {(setContext) => (
+              <div>
+                <h3>React hooks</h3>
+                <button
+                  onClick={() => {
+                    setContext({
+                      ...contextState,
+                      count: count ? count + 1 : 1,
+                    });
+                  }}
+                >
+                  click
+                </button>
+                <p>{JSON.stringify(contextState)}</p>
+              </div>
+            )}
+          </SetContext.Consumer>
+        );
+      }}
+    </Context.Consumer>
   );
 }
 
--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const defaultContext = {
+  default: "hello context!",
+  // updateContext: ()
+};
+
+export const Context = React.createContext(defaultContext);
+export const SetContext = React.createContext(() => {}); // 也可以将set的context合并在Context中，这里正好试验多个context的情况
+
+const ContextProvider = (props) => {
+  const [state, setState] = useState(defaultContext);
+
+  return (
+    <Context.Provider value={state}>
+      <SetContext.Provider value={setState}>
+        {props.children}
+      </SetContext.Provider>
+    </Context.Provider>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
